7b7d8032d132cdc4b19dab7acd519aeef78281a8
Greg <greglando@gmail.com>
2019-08-23 17:42:10 -0700

    update to setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setuptools.setup(
     name='pymemlib',
     version='r1',
     description='Windows API memory manipulation/modification library for python',
-    author='github.com/reduf, github.com/GregLando113,
+    author='github.com/reduf, github.com/GregLando113',
     url='https://github.com/GregLando113/pymemlib',
     packages=setuptools.find_packages(),
 )
